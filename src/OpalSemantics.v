3a36c0ca0cc4cef84136388dbf45a9200ad043f9
Alex Renda <alex@renda.org>
2017-11-09 23:37:25 -0500

    try prop-based semantics; currently stuck on non-strictly-positive commit

--- src/OpalSemantics.v
+++ src/OpalSemantics.v
@@ -7,87 +7,273 @@ Open Scope string_scope.
 Axiom proof_irrelevance :
   forall (P : Prop) (p q : P), p = q.
 
+Definition eq_dec_pair {A: Type} (eq_dec : forall (a1 a2: A), {a1 = a2} + {a1 <> a2})
+  : forall (aa1 aa2: A*A), {aa1 = aa2} + {aa1 <> aa2}.
+Proof.
+  decide equality.
+Qed.
 
-Module Opal (NodeMap VarMap WorldMap OpMap: SmallMap).
+Hint Resolve eq_dec_pair.
+
+Module Opal (NodeMap VarMap WorldVarMap OpMap: SmallMap).
 
   Module NodeVarMap := PairMap NodeMap VarMap.
 
   Definition node := NodeMap.key.
+  Definition node_eq_dec := NodeMap.eq_dec_key.
   Definition var := VarMap.key.
-  Definition world := WorldMap.key.
+  Definition var_eq_dec := VarMap.eq_dec_key.
+  Definition worldvar := WorldVarMap.key.
+  Definition worldvar_eq_dec := WorldVarMap.eq_dec_key.
   Definition op := OpMap.key.
+  Definition op_eq_dec := OpMap.eq_dec_key.
 
-  Inductive sexp :=
-  | EmptySet : sexp
-  | Var      : node -> var -> sexp
-  | Cons     : sexp -> sexp -> sexp.
+  Hint Resolve node_eq_dec var_eq_dec worldvar_eq_dec op_eq_dec.
 
-  Definition eq_dec_sexp : eq_dec sexp.
+  Inductive com :=
+  | SkipCom : com
+  | SeqCom : com -> com -> com
+  | IfCom : bool -> com -> com -> com
+  | WithCom : node -> com -> com
+  | AtCom : node -> com -> com
+  | HandleCom : node -> var -> op -> sexp -> var -> var -> var -> sexp -> com -> com
+  | OpCom : op -> com
+  | WorldAssignCom : worldvar -> world -> com
+  | CommitCom : world -> com
+  with
+  bool :=
+  | TrueBool : bool
+  | FalseBool : bool
+  | ConjBool : bool -> bool -> bool
+  | DisjBool : bool -> bool -> bool
+  | EqBool : sexp -> sexp -> bool
+  | MemBool : sexp -> sexp -> bool
+  with
+  sexp :=
+  | EmptySexp : sexp
+  | ConsSexp : sexp -> sexp -> sexp
+  | VarSexp : node -> var -> sexp
+  | WeightSexp : world -> node -> var -> sexp
+  with
+  world :=
+  | VarWorld : worldvar -> world
+  | HypWorld : com -> world
+  .
+
+  Fixpoint com_eq_dec (c1 c2 : com) : {c1 = c2} + {c1 <> c2}
+  with bool_eq_dec (b1 b2 : bool) : {b1 = b2} + {b1 <> b2}
+  with sexp_eq_dec (s1 s2 : sexp) : {s1 = s2} + {s1 <> s2}
+  with world_eq_dec (w1 w2 : world) : {w1 = w2} + {w1 <> w2}
+  .
   Proof.
-    unfold eq_dec.
-    specialize NodeMap.eq_dec_key.
-    specialize VarMap.eq_dec_key.
+    decide equality.
+    decide equality.
+    decide equality.
     decide equality.
   Qed.
 
-  Inductive sexp_is_value : sexp -> Prop :=
-  | EmptySetIsValue : sexp_is_value EmptySet
-  | ConsValuesIsValue : forall s1 s2,
-      sexp_is_value s1 -> sexp_is_value s2 -> sexp_is_value (Cons s1 s2).
-  Hint Constructors sexp_is_value.
+  Hint Resolve com_eq_dec bool_eq_dec sexp_eq_dec world_eq_dec.
 
-  Definition is_sexp_value : forall s, {sexp_is_value s} + {~sexp_is_value s}.
-  Proof.
-    intros.
-    induction s.
-    * left. auto.
-    * right. unfold not. intros. inversion H.
-    * destruct IHs1; destruct IHs2.
-    - auto.
-    - right. unfold not. intros. inversion H. contradiction H3.
-    - right. unfold not. intros. inversion H. contradiction H2.
-    - right. unfold not. intros. inversion H. contradiction H2.
-  Qed.
+  Section Evaluation.
+    Definition sigma_t := NodeVarMap.t sexp.
+    Definition sigma_0 : sigma_t := NodeVarMap.empty sexp sexp_eq_dec.
+    Definition sigma_eq_dec : forall s1 s2 : sigma_t, {s1 = s2} + {s1 <> s2} :=
+      NodeVarMap.eq_dec_t sexp sexp_eq_dec.
 
-  Definition sexp_value := {sexp | sexp_is_value sexp}.
+    Definition sigma_get (sigma: sigma_t) (n: node) (v: var) (s: sexp) : Prop :=
+      NodeVarMap.get sexp sexp_eq_dec sigma (n,v) s.
+    Definition sigma_set (sigma: sigma_t) (n: node) (v: var) (s: sexp) (sigma': sigma_t) : Prop :=
+      NodeVarMap.set sexp sexp_eq_dec sigma (n,v) s sigma'.
+    Definition sigma_fold {A: Type}
+               (sigma: sigma_t)
+               (f: A -> ((node*var) * sexp) -> A)
+               (init: A)
+               (res: A) : Prop :=
+      NodeVarMap.fold sexp sexp_eq_dec A f init sigma res.
 
-  Definition eq_dec_sexp_value : eq_dec sexp_value.
-  Proof.
-    unfold eq_dec.
-    intros.
-    destruct l, r.
-    destruct (eq_dec_sexp x x0).
-    * crush.
-      specialize (proof_irrelevance s s0).
-      crush.
-    * right. crush.
-Qed.
+    Definition Sigma_t := list sigma_t.
+    Inductive Sigma_get : Sigma_t -> node -> var -> sexp -> Prop :=
+    | HeadGet :
+        forall sigma Sigma n v s,
+          sigma_get sigma n v s ->
+          Sigma_get (cons sigma Sigma) n v s
+    | TailGet :
+        forall sigma Sigma n v s,
+          Sigma_get Sigma n v s ->
+          Sigma_get (cons sigma Sigma) n v s
+    .
 
-  Inductive bool :=
-  | Eq    : sexp -> sexp -> bool
-  | Mem   : sexp -> sexp -> bool
-  | Conj  : bool -> bool -> bool
-  | Disj  : bool -> bool -> bool
-  | True  : bool
-  | False : bool.
+    Definition omega_t := WorldVarMap.t (sigma_t * sigma_t).
+    Definition omega_0 : omega_t := WorldVarMap.empty (sigma_t * sigma_t) (eq_dec_pair sigma_eq_dec).
+    Definition omega_get (omega: omega_t) (u: worldvar) (sig_orig sig_hyp: sigma_t) : Prop :=
+      WorldVarMap.get (sigma_t * sigma_t) (eq_dec_pair sigma_eq_dec) omega u (sig_orig, sig_hyp).
+    Definition omega_set (omega: omega_t) (u: worldvar) (sig_orig sig_hyp: sigma_t) (omega': omega_t) : Prop :=
+      WorldVarMap.set (sigma_t * sigma_t) (eq_dec_pair sigma_eq_dec) omega u (sig_orig, sig_hyp) omega'.
 
-  Inductive bool_is_value : bool -> Prop :=
-  | TrueIsValue : bool_is_value True
-  | FalseIsValue : bool_is_value False.
+    Definition pi_t := set node.
+    Definition rho_t := node.
+    Definition eta_t := OpMap.t (node * var * sexp).
+    Definition eta_map_val_eq_dec : forall (v1 v2 : node * var * sexp),
+        {v1=v2} + {v1<>v2}.
+    Proof.
+      intros.
+      decide equality.
+       destruct a, p.
+      decide equality.
+    Qed.
+    Definition eta_get (eta: eta_t) (op: op) (n: node) (v: var) (sh: sexp) : Prop :=
+      OpMap.get (node * var * sexp) eta_map_val_eq_dec eta op (n,v,sh).
+    Definition eta_set (eta: eta_t) (op: op) (n: node) (v: var) (sh: sexp) (eta': eta_t) : Prop :=
+      OpMap.set (node * var * sexp) eta_map_val_eq_dec eta op (n,v,sh) eta'.
 
-  Definition bool_value := { b: bool | bool_is_value b}.
+    Definition mu_t := NodeVarMap.t (var * var * var * sexp).
+    Definition mu_map_val_eq_dec : forall (v1 v2 : var * var * var * sexp),
+        {v1=v2} + {v1<>v2}.
+    Proof.
+      intros.
+      decide equality.
+       destruct a, p.
+      decide equality.
+    Qed.
 
-  Inductive com :=
-  | Skip   : com
-  | Seq    : com -> com -> com
-  | If     : bool -> com -> com -> com
-  | With   : node -> com -> com
-  | At     : node -> com -> com
-  | Hyp    : world -> com -> com
-  | Commit : world -> com
-  | Handle : node -> var -> op -> sexp -> sexp -> com -> com
-  | Op     : op -> com.
+    Definition mu_get (mu: mu_t) (n: node) (v vo vh vc: var) (sm: sexp) : Prop :=
+      NodeVarMap.get (var * var * var * sexp) mu_map_val_eq_dec mu (n,v) (vo,vh,vc,sm).
+    Definition mu_set (mu: mu_t) (n: node) (v vo vh vc: var) (sm: sexp) (mu': mu_t) : Prop :=
+      NodeVarMap.set (var * var * var * sexp) mu_map_val_eq_dec mu (n,v) (vo,vh,vc,sm) mu'.
 
+    Inductive eval_sexp : sexp -> Sigma_t -> omega_t -> pi_t -> rho_t -> eta_t -> mu_t -> sexp -> Prop :=
+    | EmptyEval : forall Sigma omega pi rho eta mu,
+        eval_sexp EmptySexp Sigma omega pi rho eta mu EmptySexp
+    | ConsEval : forall s1 s1' s2 s2' Sigma omega pi rho eta mu,
+        eval_sexp s1 Sigma omega pi rho eta mu s1' ->
+        eval_sexp s2 Sigma omega pi rho eta mu s2' ->
+        eval_sexp (ConsSexp s1 s2) Sigma omega pi rho eta mu (ConsSexp s1' s2')
+    | VarEval : forall n v s Sigma omega pi rho eta mu,
+        Sigma_get Sigma n v s ->
+        eval_sexp (VarSexp n v) Sigma omega pi rho eta mu s
+    | WeightEval : forall w n v s sigma_orig sigma_hyp Sigma omega pi rho eta mu,
+        eval_world w Sigma omega pi rho eta mu sigma_orig sigma_hyp ->
+        sigma_get sigma_hyp n v s ->
+        set_In n pi ->
+        eval_sexp (WeightSexp w n v) Sigma omega pi rho eta mu s
+    with
+    eval_bool : bool -> Sigma_t -> omega_t -> pi_t -> rho_t -> eta_t -> mu_t -> bool -> Prop :=
+    | TrueEval : forall Sigma omega pi rho eta mu,
+        eval_bool TrueBool Sigma omega pi rho eta mu TrueBool
+    | FalseEval : forall Sigma omega pi rho eta mu,
+        eval_bool FalseBool Sigma omega pi rho eta mu FalseBool
+    | ConjTrueEval : forall b1 b2 b2' Sigma omega pi rho eta mu,
+        eval_bool b1 Sigma omega pi rho eta mu TrueBool ->
+        eval_bool b2 Sigma omega pi rho eta mu b2' ->
+        eval_bool (ConjBool b1 b2) Sigma omega pi rho eta mu b2'
+    | ConjFalseEval : forall b1 b2 b2' Sigma omega pi rho eta mu,
+        eval_bool b1 Sigma omega pi rho eta mu FalseBool ->
+        eval_bool b2 Sigma omega pi rho eta mu b2' ->
+        eval_bool (ConjBool b1 b2) Sigma omega pi rho eta mu FalseBool
+    | DisjFalseEval : forall b1 b2 b2' Sigma omega pi rho eta mu,
+        eval_bool b1 Sigma omega pi rho eta mu FalseBool ->
+        eval_bool b2 Sigma omega pi rho eta mu b2' ->
+        eval_bool (DisjBool b1 b2) Sigma omega pi rho eta mu b2'
+    | DisjTrueEval : forall b1 b2 b2' Sigma omega pi rho eta mu,
+        eval_bool b1 Sigma omega pi rho eta mu TrueBool ->
+        eval_bool b2 Sigma omega pi rho eta mu b2' ->
+        eval_bool (DisjBool b1 b2) Sigma omega pi rho eta mu TrueBool
+    | EqTrueEval : forall s1 s2 Sigma omega pi rho eta mu,
+        eval_sexp s1 Sigma omega pi rho eta mu EmptySexp ->
+        eval_sexp s2 Sigma omega pi rho eta mu EmptySexp ->
+        eval_bool (EqBool s1 s2) Sigma omega pi rho eta mu TrueBool
+    | EqFalseLEval : forall s1 s1l s1r s2 Sigma omega pi rho eta mu,
+        eval_sexp s1 Sigma omega pi rho eta mu (ConsSexp s1l s1r) ->
+        eval_sexp s2 Sigma omega pi rho eta mu EmptySexp ->
+        eval_bool (EqBool s1 s2) Sigma omega pi rho eta mu FalseBool
+    | EqFalseREval : forall s1 s2 s2l s2r Sigma omega pi rho eta mu,
+        eval_sexp s1 Sigma omega pi rho eta mu EmptySexp ->
+        eval_sexp s2 Sigma omega pi rho eta mu (ConsSexp s2l s2r) ->
+        eval_bool (EqBool s1 s2) Sigma omega pi rho eta mu FalseBool
+    | EqPropEval : forall s1 s1l s1r s2 s2l s2r b Sigma omega pi rho eta mu,
+        eval_sexp s1 Sigma omega pi rho eta mu (ConsSexp s1l s1r) ->
+        eval_sexp s2 Sigma omega pi rho eta mu (ConsSexp s2l s2r) ->
+        eval_bool (ConjBool (EqBool s1l s2l) (EqBool s1r s2r)) Sigma omega pi rho eta mu b ->
+        eval_bool (EqBool s1 s2) Sigma omega pi rho eta mu b
+    | MemFalseEval : forall s1 s1' s2 Sigma omega pi rho eta mu,
+        eval_sexp s1 Sigma omega pi rho eta mu s1' ->
+        eval_sexp s2 Sigma omega pi rho eta mu EmptySexp ->
+        eval_bool (MemBool s1 s2) Sigma omega pi rho eta mu FalseBool
+    | MemPropEval : forall s1 s1' s2 s2l s2r b Sigma omega pi rho eta mu,
+        eval_sexp s1 Sigma omega pi rho eta mu s1' ->
+        eval_sexp s2 Sigma omega pi rho eta mu (ConsSexp s2l s2r) ->
+        eval_bool (DisjBool (EqBool s1' s2l) (MemBool s1' s2r)) Sigma omega pi rho eta mu b ->
+        eval_bool (MemBool s1 s2) Sigma omega pi rho eta mu b
+    with
+    eval_world : world -> Sigma_t -> omega_t -> pi_t -> rho_t -> eta_t -> mu_t -> sigma_t -> sigma_t -> Prop :=
+    | LookupEval : forall u sigma_orig sigma_hyp Sigma omega pi rho eta mu,
+        omega_get omega u sigma_orig sigma_hyp ->
+        eval_world (VarWorld u) Sigma omega pi rho eta mu sigma_orig sigma_hyp
+    | HypEval : forall c sigma_hyp omega_hyp sigma Sigma omega pi rho eta mu,
+        eval_com c (cons sigma_0 (cons sigma Sigma)) omega_0 pi rho eta mu sigma_hyp omega_hyp ->
+        eval_world (HypWorld c) (cons sigma Sigma) omega pi rho eta mu sigma sigma_hyp
+    with
+    eval_com : com -> Sigma_t -> omega_t -> pi_t -> rho_t -> eta_t -> mu_t -> sigma_t -> omega_t -> Prop :=
+    | SkipEval : forall sigma Sigma omega pi rho eta mu,
+        eval_com SkipCom (cons sigma Sigma) omega pi rho eta mu sigma omega
+    | SeqEval : forall c1 c2 sigma sigma' sigma'' Sigma omega omega' omega'' pi rho eta mu,
+        eval_com c1 (cons sigma Sigma) omega pi rho eta mu sigma' omega' ->
+        eval_com c2 (cons sigma' Sigma) omega' pi rho eta mu sigma'' omega'' ->
+        eval_com (SeqCom c1 c2) (cons sigma Sigma) omega pi rho eta mu sigma'' omega''
+    | IfTrueEval : forall b c1 c2 sigma sigma' Sigma omega omega' pi rho eta mu,
+        eval_bool b (cons sigma Sigma) omega pi rho eta mu TrueBool ->
+        eval_com c1 (cons sigma Sigma) omega pi rho eta mu sigma' omega' ->
+        eval_com (IfCom b c1 c2) (cons sigma Sigma) omega pi rho eta mu sigma' omega'
+    | IfFalseEval : forall b c1 c2 sigma' Sigma omega omega' pi rho eta mu,
+        eval_bool b Sigma omega pi rho eta mu FalseBool ->
+        eval_com c2 Sigma omega pi rho eta mu sigma' omega' ->
+        eval_com (IfCom b c1 c2) Sigma omega pi rho eta mu sigma' omega'
+    | WithEval : forall n c sigma' Sigma omega omega' pi rho eta mu,
+        eval_com c Sigma omega (set_add node_eq_dec n pi) rho eta mu sigma' omega' ->
+        eval_com (WithCom n c) Sigma omega pi rho eta mu sigma' omega'
+    | AtEval : forall n c sigma' Sigma omega omega' pi rho eta mu,
+        eval_com c Sigma omega pi n eta mu sigma' omega' ->
+        eval_com (AtCom n c) Sigma omega pi rho eta mu sigma' omega'
+    | HandleEval : forall n v op sh vo vh vc sm c sigma sigma' sigma'' Sigma omega omega' pi rho eta eta' mu mu',
+        set_In n pi ->
+        sigma_set sigma n v EmptySexp sigma' ->
+        eta_set eta op n v sh eta' ->
+        mu_set mu n v vo vh vc sm mu' ->
+        eval_com c (cons sigma' Sigma) omega_0 pi rho eta' mu' sigma'' omega' ->
+        eval_com (HandleCom n v op sh vo vh vc sm c) (cons sigma Sigma) omega pi rho eta mu sigma'' omega
+    | OpEval : forall op n v sh sv sigma sigma' Sigma omega pi rho eta mu,
+        eta_get eta op n v sh ->
+        set_In n pi ->
+        eval_sexp sh (cons sigma Sigma) omega pi rho eta mu sv ->
+        sigma_set sigma n v sv sigma' ->
+        eval_com (OpCom op) (cons sigma Sigma) omega pi rho eta mu sigma' omega
+    | WorldAssign : forall u w sigma_orig sigma_hyp sigma Sigma omega omega' pi rho eta mu,
+        eval_world w (cons sigma Sigma) omega pi rho eta mu sigma_orig sigma_hyp ->
+        omega_set omega u sigma_orig sigma_hyp omega' ->
+        eval_com (WorldAssignCom u w) (cons sigma Sigma) omega pi rho eta mu sigma omega'
+    | CommitEval :
+        forall w sigma_orig sigma_hyp sigma sigma' Sigma omega pi rho eta mu,
+        eval_world w (cons sigma Sigma) omega pi rho eta mu sigma_orig sigma_hyp ->
+        (forall n v vo vc vh so sc sh sm sig_c sig_co sig_coh s,
+            mu_get mu n v vo vh vc sm ->
+            eval_sexp (VarSexp n v) (cons sigma Sigma) omega pi rho eta mu sc ->
+            sigma_set sigma_0 n vc sc sig_c ->
+            eval_sexp (VarSexp n v) (cons sigma_orig Sigma) omega pi rho eta mu so ->
+            sigma_set sig_c n vo so sig_co ->
+            sigma_get sigma_hyp n v sh ->
+            sigma_set sig_co n vh sh sig_coh ->
+            eval_sexp sm (cons sig_coh Sigma) omega pi rho eta mu s ->
+            sigma_get sigma' n v s
+        ) ->
+        (forall n v,
+            (~exists s, sigma_get sigma_hyp n v s) ->
+            forall s,
+              sigma_get sigma n v s ->
+              sigma_get sigma' n v s
+        ) ->
+        eval_com (CommitCom w) (cons sigma Sigma) omega pi rho eta mu sigma' omega
+    .
+  End Evaluation.
 
   Definition var_store := NodeVarMap.t sexp_value.
 
